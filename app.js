be5f3a2e933c6305b77e6f5c93e0f4ca4542af7f
EC2 Default User <ec2-user@ip-172-31-83-118.ec2.internal>
2019-08-23 22:40:25 +0000

    adjusted cron from ec2

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ const sync_S3 = require('./app/sync_S3')
 const CronJob = require('cron').CronJob
 // https://crontab.guru/#15_*_*_*_*
 
-new CronJob('15 * * * *', () => {
+new CronJob('45 * * * *', () => {
   sync_NS()
 }, null, true, 'America/Los_Angeles')
 
-new CronJob('45 * * * *', () => {
+new CronJob('15 * * * *', () => {
   sync_S3()
 }, null, true, 'America/Los_Angeles')
 
