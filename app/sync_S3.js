8f2a06f6ee55174afb25c81946e9789a96a2bf12
Philip Taft-Dick <philiptaft9@gmail.com>
2019-08-23 10:59:52 -0700

    building blocks for s3 upload process

--- app/sync_S3.js
+++ app/sync_S3.js
@@ -5,6 +5,15 @@ const queue = new RunQueue({
     maxConcurrency: 1
 })
 
+/* PSUEDO CODE FOR STEPS */
+/*
+(1) READ LOCAL FILE CONTENTS TO LIST
+(2) READ S3 FILE CONTENTS TO LIST
+(3) REDUCE LOCAL LIST IN COMPARISON TO REMOTE LIST
+(4) SUBMIT REDUCED LOCAL LIST TO QUEUE
+(5) RUN QUEUE
+*/
+
 let pathToFile01 = './ns_logs/gia_m2m_584636.esw3c_S.201908221200-2400-0.gz'
 let pathToFile02 = './ns_logs/gia_dsa_368164.esw3c_S.201908220000-1200-0.gz'
 let bucket = 'ns-logs-bucket'
