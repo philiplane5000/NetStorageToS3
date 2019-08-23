661bde3e8f79373e1b78a889a3fc1d2f3ca87ed2
Philip Taft-Dick <philiptaft9@gmail.com>
2019-08-23 16:31:44 -0700

    fixed ec2 bug

--- app/app.js
+++ app/app.js
@@ -1,13 +1,13 @@
-const sync_NS = require('./app/sync_NS')
-const sync_S3 = require('./app/sync_S3')
+const sync_NS = require('./sync_NS')
+const sync_S3 = require('./sync_S3')
 const CronJob = require('cron').CronJob
 // https://crontab.guru/#15_*_*_*_*
 
-new CronJob('15 * * * *', () => {
+new CronJob('45 * * * *', () => {
   sync_NS()
 }, null, true, 'America/Los_Angeles')
 
-new CronJob('45 * * * *', () => {
+new CronJob('15 * * * *', () => {
   sync_S3()
 }, null, true, 'America/Los_Angeles')
 
