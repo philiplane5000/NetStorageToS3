cd6663e444b22e7a6fc58f68f5ad45a05894ba42
EC2 Default User <ec2-user@ip-172-31-83-118.ec2.internal>
2019-08-24 22:29:32 +0000

    formatting change

--- app/s3.js
+++ app/s3.js
@@ -1,6 +1,7 @@
 const AWS = require('aws-sdk')
 
 AWS.config.update({region: 'us-east-1'})
+
 const s3 = new AWS.S3({
   accessKeyId: process.env.AWS_ACCESS_KEY,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
