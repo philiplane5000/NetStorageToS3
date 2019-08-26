75dd0b05f07b34e75da6e659f7ac2ebb40b264a7
EC2 Default User <ec2-user@ip-172-31-83-118.ec2.internal>
2019-08-26 21:36:14 +0000

    configure s3 for GIA

--- app/app.js
+++ app/app.js
@@ -3,11 +3,11 @@ const sync_S3 = require('./sync_S3')
 const CronJob = require('cron').CronJob
 // https://crontab.guru/#45_*_*_*_*
 
-new CronJob('15 * * * *', () => {
+new CronJob('29 * * * *', () => {
   sync_NS()
 }, null, true, 'America/Los_Angeles')
 
-new CronJob('45 * * * *', () => {
+new CronJob('22 * * * *', () => {
   sync_S3()
 }, null, true, 'America/Los_Angeles')
 
--- app/s3.js
+++ app/s3.js
@@ -1,8 +1,9 @@
 const AWS = require('aws-sdk')
 
-AWS.config.update({region: 'us-east-1'})
+AWS.config.update({region: 'us-west-2'})
 
 const s3 = new AWS.S3({
+  region: 'us-west-2',
   accessKeyId: process.env.AWS_ACCESS_KEY,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
 })
