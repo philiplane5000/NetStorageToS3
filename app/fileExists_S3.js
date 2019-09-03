f57bf4c1c0cb63e69fc9e042a8d9dc069824ff4d
EC2 Default User <ec2-user@ip-172-31-83-118.ec2.internal>
2019-09-03 01:48:14 +0000

    rm fileExists_S3.js

--- app/fileExists_S3.js
+++
@@ -1,7 +0,0 @@
-const utils = require('./utils')
-/* input filename to verify file upload in S3 */
-let fileName = 'gia_dsa_368164.esw3c_S.201909011200-2400-0.gz' 
-let bucket = 'akamai-netstorage-logs'
-
-utils.fileExistsS3(bucket, fileName).then(exists => console.log(exists))
-
